168b1698332090b288f428735e41ce2cb938bb9f
Abel Debebe <abeldebebe818@gmail.com>
2021-08-24 13:54:40 -0900

    initial skeleton

---
+++ src/app/Features/Home/news-feed-screen/news-feed-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-feed-screen',
+  templateUrl: './news-feed-screen.component.html',
+  styleUrls: ['./news-feed-screen.component.scss']
+})
+export class NewsFeedScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
